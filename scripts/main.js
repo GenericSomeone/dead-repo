8661cd5542b78d25c820d937e98593d3315ce817
GenericSomeone <80782836+GenericSomeone@users.noreply.github.com>
2021-04-05 07:57:20 -0700

    Removed unused script code

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,7 @@
 ﻿//use folders; don't be an idiot like me
 require("baseplate");
-//require("hypernova"); unused prototype
 require("missiles");
 require("bombs");
-//require("seaweed"); unused prototype
 require("decoys");
 
 require("unitloader/unitloader");
